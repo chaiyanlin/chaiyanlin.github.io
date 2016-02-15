046181f80b753181318ce5db781e1506ef328b22
Yanlin Chai <chai.yanlin@outlook.com>
2016-02-15 16:53:28 +0800

    modified:   gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,4 +9,12 @@ var gitMaster           = 'git checkout master',
 	hexoDeploy          = 'hexo deploy -g', //生成加部署
 	gitSyncRemoteMaster = 'git pull origin master && git push origin master';
 
-gulp.task('default', shell.task([gitMaster, gitMergeSource, hexoClean, hexoDeploy, gitSyncRemoteMaster]));
+gulp.task('default', shell.task([
+									gitMaster,
+									gitMergeSource,
+									hexoClean,
+									hexoDeploy,
+									gitSyncRemoteMaster,
+									gitSource
+								])
+		 );
