4cdb64631f416b07f297999d61f33d7760135f1a
Yanlin Chai <chai.yanlin@outlook.com>
2016-02-15 16:40:29 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp        = require('gulp'),
+	shell       = require('gulp-shell');
+
+//Cmds:
+var gitMaster           = 'git checkout master',
+	gitMergeSource      = 'git merde source',
+	hexoClean           = 'hexo clean',
+	hexoDeploy          = 'hexo deploy -g', //生成加部署
+	gitSyncRemoteMaster = 'git pull origin master && git push origin master';
+
+gulp.task('publish', shell([gitMaster, gitMergeSource, hexoClean, hexoDeploy, hexoDeploy, gitSyncRemoteMaster]));
